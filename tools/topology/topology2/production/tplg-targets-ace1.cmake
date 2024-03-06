abd76843edc03bc4f793f348a9b0b6502c975d5a
Dharageswari R <dharageswari.r@intel.com>
2024-03-06 07:31:45 -0800

    topology2: cavs-sdw: Configure RT722 on SNDW Link3 for PTL
    Signed-off-by: Dharageswari R <dharageswari.r@intel.com>

--- tools/topology/topology2/production/tplg-targets-ace1.cmake
+++ tools/topology/topology2/production/tplg-targets-ace1.cmake
@@ -22,7 +22,7 @@ SDW_DMIC_STREAM=SDW0-Capture"
 # Jack codec + SmartAmp topology. No SDW_DMIC connection
 "cavs-sdw\;sof-mtl-rt713-l0-rt1316-l12\;PLATFORM=mtl,NUM_SDW_AMP_LINKS=2,HDMI1_ID=4,HDMI2_ID=5,HDMI3_ID=6"
 
-"cavs-sdw\;sof-mtl-rt722-l0\;PLATFORM=mtl,SDW_DMIC=1,NUM_SDW_AMP_LINKS=1,\
+"cavs-sdw\;sof-mtl-rt722-l3\;PLATFORM=mtl,SDW_DMIC=1,NUM_SDW_AMP_LINKS=1,\
 SDW_AMP_FEEDBACK=false,SDW_SPK_STREAM=Playback-SmartAmp,SDW_DMIC_STREAM=Capture-SmartMic,\
 SDW_JACK_OUT_STREAM=Playback-SimpleJack,SDW_JACK_IN_STREAM=Capture-SimpleJack,DEEP_BUF_SPK=true"
 
